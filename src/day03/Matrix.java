af154df12e5e74cf198e2d7d9339cfa2c01c2958
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-29 11:51:44 +0900

    martrix

--- src/day03/Matrix.java
+++ src/day03/Matrix.java
@@ -39,12 +39,58 @@ public class Matrix {
         int[] arr3 = arr2d[2];
 
         //2차원 배열의 순회
-        for (int[] arr  :arr2d){
-            for (int n :arr){
+        for (int[] arr : arr2d){
+            for (int n : arr){
                 System.out.printf("%3d ",n );
             }
             System.out.println();
         }
+
+        //배열의 기본값
+        //정수배열은 0이 기본값, 실수는 0.0.이 기본값
+        //논리는 거짓이 기본, char배열은 기본값이 공백
+        // 그 나머지는 전부 null이 기본
+        char[] numbers = new char[5];
+        System.out.println("=========================");
+        System.out.println(Arrays.toString(numbers));
+
+        //5행 4열의 2차원배열을 생성만하고 싶다.
+        int[][] array2d = new int[5][4];
+
+        //2차원 배열의 수정
+
+        //array2d  2행 4열 값 수정
+        array2d[1][3] = 100;
+        array2d[2][1] = 55;
+        array2d[3] = new int[] {9, 8, 7, 6};
+
+        System.out.println("==========================");
+        for (int[] ints : array2d){
+            for (int anInt : ints){
+                System.out.printf("%3d ", anInt);
+            }
+            System.out.println();
+        }
+
+        //3차원 배열
+        int[][][] arr3d = {
+                {
+                        {10,20},
+                        {30,40}
+                },
+                {
+                        {50,60},
+                        {70,80}
+                },
+                {
+                        {90,100},
+                        {200,300}
+                }
+        };
+
+        System.out.println(arr3d[1][0][1]);
+
+
 
 
 
