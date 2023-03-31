898cb2d0331de86e82e195f2166787745b69baab
Yajani <115179558+Yajani@users.noreply.github.com>
2023-03-31 11:10:33 +0900

    dya05커밋

---
+++ src/day05/member/Test.java
@@ -0,0 +1,14 @@
+package day05.member;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        MemberRepository mr = new MemberRepository();
+
+        new Member(4, )
+        mr.showMembers();
+
+
+    }
+}
