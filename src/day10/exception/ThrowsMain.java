b1f8d53573d11977b3f10ecf81ad38fd6edd63ca
Yajani <115179558+Yajani@users.noreply.github.com>
2023-04-07 10:32:24 +0900

    예외처리

---
+++ src/day10/exception/ThrowsMain.java
@@ -0,0 +1,11 @@
+package day10.exception;
+
+import day09.abs.Main;
+
+public class ThrowsMain {
+    public static void main(String[] args) {
+        ThrowsExample te =new ThrowsExample();
+        int n = te.inputNumber();
+        int m = te.convert("100");
+    }
+}
