bc198b2fb82f13e8bf553dbd7f62909c0c716eb2
Yajani <115179558+Yajani@users.noreply.github.com>
2023-04-07 12:14:45 +0900

    generic

---
+++ src/day10/collection/list/ListExample.java
@@ -0,0 +1,15 @@
+package day10.collection.list;
+
+import java.sql.Array;
+import java.util.*;
+
+public class ListExample {
+    /*
+    * -저장 데이터의 순서를 보장
+    * -인덱스를 가지고 있음
+    * -중복 데이터를 저장가능
+    * */
+    public static void main(String[] args) {
+        List<String> foodList = new List<String>();
+    }
+}
